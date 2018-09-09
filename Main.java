256a37d810b1949070192fad6929bcd20a2c33a1
Nicholas Severino <42677914+severinonicholas2019@users.noreply.github.com>
2018-09-09 13:11:13 -0700

    coded poem
    This is the famous l(a poem in one line of code.

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("l(a"); System.out.println(" "); System.out.println("le");System.out.println("af");System.out.println("fa");System.out.println("ll");System.out.println(" ");System.out.println(" ");System.out.println("s)");System.out.println("one");System.out.println(" ");System.out.println("l");System.out.println("iness");
+    }
+}
