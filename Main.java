e92177a8f43760b5bbd19d2671ba33625dcc96c0
Nicholas Severino <42677914+severinonicholas2019@users.noreply.github.com>
2018-09-10 13:32:50 -0700

    Remaking of the l(a
    I made the code more simplified and condensed

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("l(a"); System.out.println(" "); System.out.println("le");System.out.println("af");System.out.println("fa");System.out.println("ll");System.out.println(" ");System.out.println(" ");System.out.println("s)");System.out.println("one");System.out.println(" ");System.out.println("l");System.out.println("iness");
+        System.out.println("l(a\n \nle\naf\nfa\nll\n\ns)\none\n\nl\niness");
     }
 }
