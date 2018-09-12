e880a321410bcfab72c23d91eaf425809b230d6c
Nicholas Severino <42677914+severinonicholas2019@users.noreply.github.com>
2018-09-12 09:08:11 -0700

    Update Main.java
    correct code

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("l(a\n \nle\naf\nfa\nll\n\ns)\none\n\nl\niness");
+        System.out.println("l(a\n \nle\naf\nfa\nll\n\ns)\none\nl\n\niness");
     }
 }
